352979a54028abc3889e84275287cce3f3c1c8fd
James Alley <manalagi001@yahoo.com>
2011-12-01 19:14:37 -0800

    Tons of new options...

--- firstInsight/drawer.js
+++
@@ -1,8 +0,0 @@
-YUI().use('node','event', function (Y) {
-
-    Y.one('.drawer').on('click',function() {
-        Y.one('body').toggleClass('drawer-closed');
-        //this.toggleClass('closed');
-    });
-
-});
