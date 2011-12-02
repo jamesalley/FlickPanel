5a12659a9c71b93e4b28ac52445c05e4ffc1c8bb
James Alley <manalagi001@yahoo.com>
2011-12-02 14:33:54 -0800

    Added body flick

--- scrollaway/drawer.js
+++ scrollaway/drawer.js
@@ -1,8 +1,30 @@
-YUI().use('node','event', function (Y) {
+YUI().use('node','event','event-outside','event-flick', function (Y) {
 
-    Y.one('.drawer').on('click',function() {
-        Y.one('body').toggleClass('drawer-closed');
+    Y.Event.defineOutside('touchend');
+
+    var drawer = Y.one('.drawer'),
+        classTarget = Y.one('body');
+    
+    drawer.on('click',function() {
+        classTarget.toggleClass('drawer-closed');
         //this.toggleClass('closed');
+    });
+    
+    drawer.on('clickoutside',function(){
+        classTarget.addClass('drawer-closed');
+    });
+    
+    classTarget.on('flick',function(e){
+        var flick = e.flick;
+        if (flick.axis === 'x') {
+            if (flick.distance > 10) {
+                classTarget.removeClass('drawer-closed');
+            } 
+            else if (flick.distance < -10) {
+                classTarget.addClass('drawer-closed');
+            } 
+        }
+    
     });
 
 });
