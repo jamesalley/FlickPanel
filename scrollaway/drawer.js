d24bc2f69b417919b78751f27c995d2b72bccecc
James Alley <manalagi001@yahoo.com>
2011-12-02 17:50:18 -0800

    Best version of scrollaway yet
    This version incorporates a body flick, a scrolling rail, and a rail
    that "locks" to the top of the page when the page is flicked up.

--- scrollaway/drawer.js
+++ scrollaway/drawer.js
@@ -6,7 +6,7 @@ YUI().use('node','event','event-outside','event-flick', function (Y) {
         classTarget = Y.one('body');
     
     drawer.on('click',function() {
-        classTarget.toggleClass('drawer-closed');
+        //classTarget.toggleClass('drawer-closed');
         //this.toggleClass('closed');
     });
     
@@ -27,4 +27,24 @@ YUI().use('node','event','event-outside','event-flick', function (Y) {
     
     });
 
+    drawer.on('scroll',function(){
+        Y.log('drawer is scrolling');
+    }, Y.config.win,this);
+
+    this._drawerStop = function() {
+        //Y.log('win is scrolling');
+        var yVal = Y.one('header').get('offsetHeight'),
+            win = Y.config.win;
+        // if the window scrolls past the height of the header...
+        if (win.scrollY >= yVal) {
+            drawer.setStyle('position','fixed');
+            drawer.setStyle('height',win.innerHeight);
+        } else {
+            drawer.setStyle('position','');
+        }
+    }
+    
+    Y.on('touchmove',this._drawerStop, Y.config.win, this);
+    Y.on('scroll',this._drawerStop, Y.config.win, this);
+    
 });
